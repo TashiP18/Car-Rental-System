acbfbb036902771bd7e0f797ebe6d7da36f48816
Tt987644 <987644@win.edu.au>
2025-05-10 18:06:18 +1000

    logout.php

---
+++ logout.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+session_destroy();
+header('Location: index.php');
+exit();
+?>
+
+
+<!-- background: url('https://images.unsplash.com/photo-1503376780353-7e6692767b70?auto=format&fit=crop&w=1950&q=80') no-repeat center center;           -->
+
+
